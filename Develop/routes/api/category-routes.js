b46d9215dcc43e197ac6a6421a17efe3303da59b
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-01-11 22:33:49 -0500

    created post and put  methods for rpues/api/category-routes.js nees to create delete still

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -40,7 +40,7 @@ router.get('/:id', (req, res) => {
   })
   .then(categoryDB => {
     if (!categoryDB) {
-      res.status(404).json({ message: 'No Category found with this id' });
+      res.status(404).json({ message: 'No Category was found with given id!' });
       return;
   }
     res.json(categoryDB)
@@ -53,10 +53,41 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(categoryDB => res.json(categoryDB))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
+
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update( 
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    } 
+  )
+  .then(categoryDB => {
+    if (!categoryDB) {
+        res.status(404).json({ message: 'No Category was found with given id!' });
+        return;
+    }
+    res.json(categoryDB);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+
 });
 
 router.delete('/:id', (req, res) => {
